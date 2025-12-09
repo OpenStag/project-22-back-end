b2190ba65cb2b89a0913102f038878f2733adf86
Janith Wathsala <janijaniwathsala@gmail.com>
2025-12-09 12:25:00 +0530

    initional project

---
+++ src/test/java/com/openstage/ticketbook/TicketbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openstage.ticketbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
