3fb1973018fd10f5d39ff3219f006d0dcfcb6a2d
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-11 00:11:27 +0530

    feat(repository): enforce non-null constraints on User and Long in UserRepository

--- src/main/java/com/openstage/ticketbook/repo/UserRepository.java
+++ src/main/java/com/openstage/ticketbook/repo/UserRepository.java
@@ -1,13 +1,14 @@
 package com.openstage.ticketbook.repo;
 
 import com.openstage.ticketbook.model.User;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<@NonNull User, @NonNull Long> {
     // SELECT COUNT(*) > 0 FROM users WHERE email = ?
     boolean existsByEmail(String email);
 
