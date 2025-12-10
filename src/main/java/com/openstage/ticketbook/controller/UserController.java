0104efb7b5725d893c06c8b69d46ffa24645f8f5
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-11 00:10:43 +0530

    feat(login): add login endpoint to UserController for user authentication

--- src/main/java/com/openstage/ticketbook/controller/UserController.java
+++ src/main/java/com/openstage/ticketbook/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.openstage.ticketbook.controller;
 
-import com.openstage.ticketbook.dto.UserRequestDTO;
-import com.openstage.ticketbook.dto.UserResponseDTO;
+import com.openstage.ticketbook.dto.*;
 import com.openstage.ticketbook.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,6 +34,35 @@ public class UserController {
         } catch (RuntimeException e) {
             // Error: Return 400 Bad Request and the error message string
             return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    // Login Endpoint
+    @PostMapping("/login")
+    @Operation(summary = "Login a User")
+    public ResponseEntity<@NonNull ResponseDTO<LoginResponseDTO>> login(
+            @RequestBody @Valid LoginRequestDTO request,
+            HttpServletRequest httpServletRequest) {
+
+        try {
+            LoginResponseDTO response = userService.loginUser(request);
+
+            // Create a session
+            HttpSession newSession = httpServletRequest.getSession(true);
+            newSession.setAttribute("USER_ID", response.getId());
+            newSession.setAttribute("USERNAME", response.getUsername());
+            newSession.setAttribute("EMAIL", response.getEmail());
+            newSession.setAttribute("ROLE", response.getRole());
+
+            return ResponseEntity.status(HttpStatus.OK).body(
+                    new ResponseDTO<>(true, "Login successful", response)
+            );
+
+        } catch (RuntimeException e) {
+            // need to send unauthorized status with message
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
+                    new ResponseDTO<>(false, e.getMessage(), null)
+            );
         }
     }
 }
