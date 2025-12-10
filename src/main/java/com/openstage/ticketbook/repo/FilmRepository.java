f919575ea6dde88f1925c5851081d5ba99e9e2e6
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-10 22:26:54 +0530

    feat(Film): enforce non-null constraints on Film and Long types in FilmRepository

--- src/main/java/com/openstage/ticketbook/repo/FilmRepository.java
+++ src/main/java/com/openstage/ticketbook/repo/FilmRepository.java
@@ -1,9 +1,10 @@
 package com.openstage.ticketbook.repo;
 
 import com.openstage.ticketbook.model.Film;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FilmRepository extends JpaRepository<Film, Long> {
+public interface FilmRepository extends JpaRepository<@NonNull Film, @NonNull Long> {
 }
