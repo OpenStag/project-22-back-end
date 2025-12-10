b4bbc941aa8c5e0eb7283886037d0268882cc868
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-10 13:49:35 +0530

    feat(Film): update FilmRequestDTO to use @NotNull for date, time, and price fields

--- src/main/java/com/openstage/ticketbook/dto/FilmRequestDTO.java
+++ src/main/java/com/openstage/ticketbook/dto/FilmRequestDTO.java
@@ -1,6 +1,7 @@
 package com.openstage.ticketbook.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,21 +22,21 @@ public class FilmRequestDTO {
     @NotBlank(message = "Film description is required")
     private String filmDescription;
 
-    @NotBlank(message = "Film date is required")
+    @NotNull(message = "Film date is required")
     private LocalDate filmDate;
 
-    @NotBlank(message = "Film time is required")
+    @NotNull(message = "Film time is required")
     private LocalTime filmTime;
 
     private String imageUrl;
 
     // Price details (Adding ticket prices for the film)
-    @NotBlank(message = "Level 1 price is required")
+    @NotNull(message = "Level 1 price is required")
     private Double l1Price;
 
-    @NotBlank(message = "Level 2 price is required")
+    @NotNull(message = "Level 2 price is required")
     private Double l2Price;
 
-    @NotBlank(message = "Box price is required")
+    @NotNull(message = "Box price is required")
     private Double boxPrice;
 }
