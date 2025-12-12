3a91a098bd806fce1663324e448fd4d935f41faf
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-12 23:43:27 +0530

    feat(film): enhance FilmController with methods to add, retrieve upcoming, and get all films

--- src/main/java/com/openstage/ticketbook/controller/FilmController.java
+++ src/main/java/com/openstage/ticketbook/controller/FilmController.java
@@ -2,6 +2,7 @@ package com.openstage.ticketbook.controller;
 
 import com.openstage.ticketbook.dto.FilmRequestDTO;
 import com.openstage.ticketbook.dto.FilmResponseDTO;
+import com.openstage.ticketbook.dto.ResponseDTO;
 import com.openstage.ticketbook.service.FilmService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -10,10 +11,9 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/films")
@@ -22,12 +22,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class FilmController {
     private final FilmService filmService;
 
+    // Add a new Film
     @PostMapping
     @Operation(summary = "Add a new film")
-    public ResponseEntity<@NonNull FilmResponseDTO> addFilm(
+    public ResponseEntity<@NonNull ResponseDTO<FilmResponseDTO>> addFilm(
             @Valid @RequestBody FilmRequestDTO request
     ) {
         FilmResponseDTO response = filmService.addFilm(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                new ResponseDTO<>(true, "Film added successfully", response)
+        );
+    }
+
+    // Get Upcoming Films
+    @GetMapping("/upcoming")
+    @Operation(summary = "Get upcoming films")
+    public ResponseEntity<@NonNull ResponseDTO<List<FilmResponseDTO>>> getUpcomingFilms() {
+        List<FilmResponseDTO> films = filmService.getUpcomingFilms();
+        return ResponseEntity.ok(
+                new ResponseDTO<>(true, "Upcoming films retrieved successfully", films)
+        );
+    }
+
+    // Get All Films
+    @GetMapping
+    @Operation(summary = "Get all films")
+    public ResponseEntity<@NonNull ResponseDTO<List<FilmResponseDTO>>> getAllFilms() {
+        List<FilmResponseDTO> films = filmService.getAllFilms();
+        return ResponseEntity.ok(
+                new ResponseDTO<>(true, "All films retrieved successfully", films)
+        );
     }
 }
