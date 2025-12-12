a5b64ffc3700e46aa966412385f22462308f2e18
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-12 23:43:04 +0530

    feat(film): add method to find films by date and order by film date and time

--- src/main/java/com/openstage/ticketbook/repo/FilmRepository.java
+++ src/main/java/com/openstage/ticketbook/repo/FilmRepository.java
@@ -5,6 +5,14 @@ import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface FilmRepository extends JpaRepository<@NonNull Film, @NonNull Long> {
+    // Find films with filmDate greater than or equal to currentDate, ordered by filmDate and filmTime
+    // 1st - Find By filmDate Greater Than Equal to currentDate
+    // 2nd - Order By filmDate Asc filmTime Asc
+    @NonNull
+    List<@NonNull Film> findByFilmDateGreaterThanEqualOrderByFilmDateAscFilmTimeAsc(LocalDate currentDate);
 }
